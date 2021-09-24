2d817e29f5dfbf5d040daa10d2fc4e8aeb53bd54
ggalanteCode <ggalante3@gmail.com>
2021-09-24 15:08:04 +0200

    javadoc comment for the CodeShovelParser class added;getAnalyzableFile method created; constructor created; class fields added; execute method created

--- src/Parsers/CodeShovelParser.java
+++ src/Parsers/CodeShovelParser.java
@@ -1,13 +1,35 @@
 package Parsers;
 
+import java.io.BufferedReader;
+
 /**
- *
+ * this class parses the results in the output file produced by the tool for method-level source code histories
+ * (CodeShovel) and writes the parsed results on the 'sbac' database
  * @author Giovanni Galante
  */
 public class CodeShovelParser {
 
-    public CodeShovelParser(String commitId, String URLProject) {
+    public String getAnalyzableFile() {
+        return analyzableFile;
+    }
 
+    private String analyzableFile = "results.json";
+    private String urlProject;
+    private String commitId;
+
+    public CodeShovelParser(String commitId, String URLProject) {
+        this.commitId = commitId;
+        this.urlProject = URLProject;
+    }
+
+    /**
+     * reads the output file using a buffered reader
+     * @param bufferedReader the buffered reader
+     */
+    public void execute(BufferedReader bufferedReader) {
+        //try {
+
+        //}
     }
 
 }
