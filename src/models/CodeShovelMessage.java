588991b7fa06c3175db7db2fbe81fa4cb41e2a65
ggalanteCode <ggalante3@gmail.com>
2021-09-24 14:47:09 +0200

    CodeShovelMessage.java created

---
+++ src/models/CodeShovelMessage.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * an instance of this class for the content of the CodeShovel's JSON output file
+ * @author Giovanni Galante
+ */
+public class CodeShovelMessage {
+
+
+
+}
